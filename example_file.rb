a4c48d35716c513532c2d8aefce581c1d0defa71
Sara Tibbetts <sxt4866@rit.edu>
2018-02-13 20:12:10 -0500

    adds new feature method

--- example_file.rb
+++ example_file.rb
@@ -14,3 +14,10 @@ puts "New feature"
 puts "Does this work?"
 
 puts "i added this"
+
+#######################################
+# New feature on branch
+#######################################
+def my_new_feature
+  puts "Hello world"
+end
